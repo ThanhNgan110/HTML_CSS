0dab557b56cacd3245828de84a2118ac0e6639a1
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-10-17 16:46:56 +0700

    Update handle slider

--- src/script/index.js
+++ src/script/index.js
@@ -1,51 +1,56 @@
-const listImages = document.querySelector(".carousel-inner"); // Parent container of all slides
-const imgs = document.querySelectorAll(".carousel-item"); // Select all carousel items
-const length = imgs.length; // Number of slides
+const listImages = document.querySelector(".carousel-item");
+const imgs = document.querySelectorAll(".img");
+
+const length = imgs.length;
 let current = 0;
 
-// Right and left buttons
 const btnRight = document.getElementById("btn-right");
 const btnLeft = document.getElementById("btn-left");
 
 let interval;
 
-// Function to change the slide
 const handleChangeSlide = () => {
-  if (current === length - 1) {
-    current = 0; // Reset to first slide
+  if (current == length - 1) {
+    current = 0;
+    let width = imgs[0].offsetWidth;
+    listImages.style.transform = `translateX(0px)`;
   } else {
-    current++; // Go to next slide
+    current++;
+    let width = imgs[0].offsetWidth;
+    console.log("width", width);
+
+    listImages.style.transform = `translateX(${width * -1 * current}px)`;
   }
-  let width = imgs[0].offsetWidth; // Get the width of one slide
-  listImages.style.transform = `translateX(${-width * current}px)`; // Move the slides container
 };
 
-// Function to start the auto-slide
 const startAutoSlide = () => {
   interval = setInterval(handleChangeSlide, 4000); // Start changing slides every 4 seconds
 };
 
-// Function to stop the auto-slide
 const stopAutoSlide = () => {
   clearInterval(interval); // Clear the interval to stop auto-slide
 };
 
-// Right button click handler
+// // Start the auto-slide when the page loads
+// startAutoSlide();
+
 btnRight.addEventListener("click", () => {
-  stopAutoSlide(); // Stop the auto-slide when clicking manually
-  handleChangeSlide(); // Go to the next slide
-  startAutoSlide(); // Restart the auto-slide after manual change
+  stopAutoSlide();
+  handleChangeSlide();
+  startAutoSlide();
 });
 
-// Left button click handler
 btnLeft.addEventListener("click", () => {
-  stopAutoSlide(); // Stop the auto-slide when clicking manually
-  if (current === 0) {
-    current = length - 1; // Go to last slide
+  stopAutoSlide();
+  if (current == 0) {
+    current = length - 1;
+    let width = imgs[0].offsetWidth;
+    listImages.style.transform = `translateX(${width * -1 * current}px)`;
   } else {
-    current--; // Go to previous slide
+    current--;
+    let width = imgs[0].offsetWidth;
+    console.log("width", width);
+    listImages.style.transform = `translateX(${width * -1 * current}px)`;
   }
-  let width = imgs[0].offsetWidth;
-  listImages.style.transform = `translateX(${-width * current}px)`; // Move slides container
-  startAutoSlide(); // Restart the auto-slide after manual change
+  startAutoSlide();
 });
